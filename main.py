f92dabc22dde0c7a5e5f29cb26812c2838b7acb6
AdamQ123585 <qureshiada@students.dsbn.org>
2025-03-24 12:11:26 -0400

    Finished

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+# Name: Adam Qureshi
+# Title: 100 Random Number Averager
+# Date: Feb 20, 2024
+# This Program lists 100 random numbers and then averages them
+
+import random
+
+Numbers = [random.randint(0,100) for i in range(100)]
+
+print ("The 100 numbers are:")
+print (Numbers)
+
+Average = sum(Numbers)/len(Numbers)
+
+print ("The Average of your numbers is:")
+print (Average)
+
 
